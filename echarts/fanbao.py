8de1f53692ef2bcdc6fd219915172c6660016141
qianhangkang <qianhangkang@dxy.cn>
2021-12-29 22:40:16 +0800

    反包页面增加股票代码

--- echarts/fanbao.py
+++ echarts/fanbao.py
@@ -1,7 +1,8 @@
 from time import sleep
 
-import pyecharts.options as opts
-from pyecharts.charts import Line
+from pyecharts import options as opts
+from pyecharts.charts import Line, Page
+from pyecharts.components import Table
 
 from stock_basic import east_assert as eas
 from stock_basic import tushare_api as ta
@@ -37,19 +38,7 @@ def get_fanbao_dict():
     return fanbao_dict
 
 
-def draw_charts(fanbao_dict: dict):
-    """
-    Gallery 使用 pyecharts 1.1.0
-    参考地址: https://echarts.apache.org/examples/editor.html?c=line-simple
-
-    目前无法实现的功能:
-
-    暂无
-    """
-
-    # x_data = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    # y_data = [820, 932, 901, 934, 1290, 1330, 1320]
-    print("draw chrats...")
+def line_smooth(fanbao_dict: dict) -> Line:
     x_data = []
     y_data = []
     for k, v in fanbao_dict.items():
@@ -70,7 +59,47 @@ def draw_charts(fanbao_dict: dict):
                 type_="value",
                 axistick_opts=opts.AxisTickOpts(is_show=True),
                 splitline_opts=opts.SplitLineOpts(is_show=True)))
-            .render("line_smooth.html")
+        # .render("line_smooth.html")
     )
+
+    return c
+
+
+def table_fanbao(fanbao_dict: dict) -> Table:
+    table = Table()
+
+    headers = ["日期", "股票代码"]
+    # rows = [
+    #     ["Brisbane", 5905, 1857594, 1146.4],
+    #     ["Adelaide", 1295, 1158259, 600.5],
+    #     ["Darwin", 112, 120900, 1714.7],
+    #     ["Hobart", 1357, 205556, 619.5],
+    #     ["Sydney", 2058, 4336374, 1214.8],
+    #     ["Melbourne", 1566, 3806092, 646.9],
+    #     ["Perth", 5386, 1554769, 869.4],
+    # ]
+    rows = []
+    for k, v in fanbao_dict.items():
+        code = ','.join(v)
+        item = [k, code]
+        rows.append(item)
+
+    table.add(headers, rows).set_global_opts(
+        title_opts=opts.ComponentTitleOpts(title="Table")
+    )
+    return table
+
+
+def draw_charts(fanbao_dict: dict):
+    line = line_smooth(fanbao_dict)
+    table = table_fanbao(fanbao_dict)
+    print("draw chrats...")
+
+    page = Page(layout=Page.SimplePageLayout)
+    page.add(
+        line,
+        table
+    )
+    page.render('line_smooth.html')
 
     print("done ...")
--- test/test_east_assert.py
+++ test/test_east_assert.py
@@ -1,8 +1,4 @@
-from stock_basic.tushare_api import get_trade_cal
+from echarts.fanbao import draw_charts, get_fanbao_dict
 
 if __name__ == '__main__':
-    # df = ea.get_top('20211227')
-    # print(len(df))
-    # print(df.head())
-    trade_df = get_trade_cal()
-    print(trade_df)
+    draw_charts(get_fanbao_dict())
